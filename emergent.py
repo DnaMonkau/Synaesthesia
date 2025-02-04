4afbb6db27a49c2197517a9b378cac1e357e90d7
DnaMonkau <106347125+DnaMonkau@users.noreply.github.com>
2025-02-04 15:30:21 +0100

    Update emergent.py

--- emergent.py
+++ emergent.py
@@ -38,17 +38,27 @@ class SimpleSynaesthesiaNet(nn.Module):
     self.g = nn.Sigmoid()
     # debatable whether M or modalities
     if cross_talk:
-      self.K = nn.Parameter(torch.zeros(self.modalities, self.modalities) + np.random.choice([-0.1,0.1])) # start at 0 fixed point or near 0
+      # Generate random angles
+      theta = 2 * np.pi * torch.rand(1)
+
+      # Set radius for the ring
+      radius = 0.1  # Can be adjusted
+
+      # Convert to cartesian coordinates
+      K12 = radius * torch.cos(theta)
+      K21 = radius * torch.sin(theta)
+
+      self.K = nn.Parameter(torch.zeros(self.M, self.M) + torch.tensor([K21, K12]), requires_grad=False) # start at 0 fixed point or near 0
       with torch.no_grad():
         self.K.fill_diagonal_(0) # no self connections
     else:
-      self.K = nn.Parameter(torch.zeros(self.modalities, self.modalities), requires_grad=False) # start at 0 fixed point
+      self.K = nn.Parameter(torch.zeros(self.M, self.M), requires_grad=False) # start at 0 fixed point
 
-    self.W1 = nn.Parameter(torch.randn(self.M, self.N), requires_grad=True)
-    self.W2 = nn.Parameter(torch.randn(self.M, self.N), requires_grad=True)
+    self.W1 = nn.Parameter(torch.randn(int(self.M/2), self.N), requires_grad=True)
+    self.W2 = nn.Parameter(torch.randn(int(self.M/2), self.N), requires_grad=True)
 
-    self.s1 = torch.zeros((self.M), requires_grad=False) # Membrane potential or output neurons
-    self.s2 = torch.zeros((self.M), requires_grad=False) # Membrane potential or output neurons
+    self.s1 = torch.zeros((int(self.M/2)), requires_grad=False) # Membrane potential or output neurons
+    self.s2 = torch.zeros((int(self.M/2)), requires_grad=False) # Membrane potential or output neurons
     self.optimizer = torch.optim.Adam(self.parameters(), lr=eta)
     self.spikes= None
     self.losses =  None
@@ -62,8 +72,8 @@ class SimpleSynaesthesiaNet(nn.Module):
     # Apply first order activities to the output perception s
     # self.s[0] = self.g(self.W[0] @ x[0] + self.K[0] @ self.s[1])
     # self.s[1] = self.g(self.W[1] @ x[1] + self.K[1] @ self.s[0]) #
-    self.s1 = self.g(self.W1 @ x[0] + self.K[0][1].clone() * s2)
-    self.s2 = self.g(self.W2 @ x[1] + self.K[1][0].clone() * s1)
+    self.s1 = self.g(self.W1 * x[0] + self.K[0][1].clone() * s2)
+    self.s2 = self.g(self.W2 * x[1] + self.K[1][0].clone() * s1)
     return
 
   def feedforward_dynamics(self, x, t, s1, s2, samp_ts):
@@ -71,11 +81,6 @@ class SimpleSynaesthesiaNet(nn.Module):
     The feedforward dynamics  of the model
     note: original math and paper do not consider modalities
     '''
-    # perform update step per modality
-    # s_delta1 = -s1 + self.g(self.W1 @ x[0] + self.K[0][1].clone() * s2)
-    # s_delta2 = -s2 + self.g(self.W2 @ x[1] + self.K[1][0].clone() * s1)
-    # self.s1 = s1 + (s_delta1 / self.tau)
-    # self.s2 = s2 + (s_delta2 / self.tau)
     s = torch.stack([s1, s2])
     self.s1, self.s2 = odeint(self.dynamics, s, samp_ts[:t+1])[-1]
 
@@ -113,34 +118,6 @@ class SimpleSynaesthesiaNet(nn.Module):
     return E1, E2, chi_1, chi_2, G_1, G_2, phi_1, phi_2
 
   def learning_rule(self, x, chi_1, chi_2, G_1, G_2, phi_1, phi_2):
-    # # analytical
-    # g_p_1 = self.logistic_derivative(self.W1 @ x[0] + self.K[0][1] * self.s2)
-    # g_p_2 = self.logistic_derivative(self.W2 @ x[1] + self.K[1][0] * self.s1)
-    # g_pp_1 = self.second_logistic_derivative(self.W1 @ x[0] + self.K[0][1] * self.s2)
-    # g_pp_2 = self.second_logistic_derivative(self.W2 @ x[1] + self.K[1][0] * self.s1)
-    # g_p_s = [g_p_1, g_p_2]
-    # g_pp_s = [g_pp_1, g_pp_2]
-    # g_p_x = self.logistic_derivative(x)
-    # g_pp_x = self.second_logistic_derivative(x)
-    # G = [G_1, G_2]
-    # # q = 1/ 1- g_p[0] * g_p[1] * self.K[0][1] * self.K[1][0]
-    # z = g_pp_x/g_p_x
-
-    # a  = torch.zeros(2)
-    # chi = [chi_1, chi_2]
-    # phi = [phi_1, phi_2]
-    # k_delta = torch.zeros(self.modalities, self.M)
-    # s = [self.s1, self.s2]
-    # for m in range(self.modalities):
-    #   Gamma = torch.linalg.inv(chi[m].T @ chi[m]) @ chi[m].T @ phi[m]
-    #   a = self.dynamics_a(chi[m], Gamma,g_p_s[m], g_pp_s[m])
-    #   k_delta[m] = self.eta * torch.mean((chi[m] @ Gamma).T +G[m].T@a@s[m].T)
-    # # k_delta1 = self.eta * torch.mean(q * g_p[0] * g_p[1] * self.K[1][0] + q**2 * (g_pp[0]/g_p[0] + (g_p[0]*g_pp[1])*self.K[1][0]/g_p[1])*self.s[1])
-    # # k_delta2 = self.eta * torch.mean(q * g_p[0] * g_p[1] * self.K[0][1] + q**2 * (g_pp[1]/g_p[1] + (g_p[1]*g_pp[0])*self.K[0][1]/g_p[0]*self.s[0]))
-    # w_delta1 = self.eta * ((self.W1)**(-1) + torch.mean(z@x.T))
-    # w_delta2 = self.eta * ((self.W2)**(-1) + torch.mean(z@x.T))
-    # E = self.objective_function(x)
-
     ## numerical calculation with autgrad
     G = [G_1, G_2]
     # print(np.shape(torch.linalg.inv(G[0]**(-1) - np.shape(self.W1))
@@ -161,65 +138,40 @@ class SimpleSynaesthesiaNet(nn.Module):
       self.W1 = nn.Parameter(self.W1.clone() + w_delta1)
       self.W2  = nn.Parameter(self.W2.clone() + w_delta2)
     if self.cross_talk:
-      k_delta = - self.eta * torch.autograd.grad(torch.mean(torch.stack([E1, E2]), 0), self.K, retain_graph=True)[0]
+      # k_delta = - self.eta * torch.autograd.grad(torch.stack([E1, E2]), self.K, retain_graph=True)[0]
+      k_delta1  = self.eta * torch.mean(self.s2) * torch.mean(1-2*self.s1)
+      k_delta2 = self.eta * torch.mean(self.s1) * torch.mean(1-2*self.s2)
       with torch.no_grad():
-        self.K = nn.Parameter(self.K.clone() + k_delta)
+        # self.K = nn.Parameter(self.K.clone() + k_delta)
+        self.K[0][1] += k_delta1
+        self.K[1][0] += k_delta2
         self.K.fill_diagonal_(0)
-        #   self.K[0][1] += torch.mean(k_delta[0])
-        #   self.K[1][0] += torch.mean(k_delta[1])
     return
 
 
   def convergence(self, x, s1, s2):
-    # g_p_1 = self.logistic_derivative(x[0])
-    # g_p_2 = self.logistic_derivative(x[1])
-    # g_pp_1 = self.second_logistic_derivative(x[0])
-    # g_pp_2 = self.second_logistic_derivative(x[1])
-    # g_ppp_1 = self.third_logistic_derivative(x[0])
-    # g_ppp_2 = self.third_logistic_derivative(x[1])
-    # partial_delta_f2_k12 = partial_delta_f1_k21 = -2 * s2 @ s1**2 + s1**2 @ s2**2
-    # partial_delta_f1_k12 = -2 * s1 @ s2 + s1**2 @ s2**2
-
-    # partial_delta_f2_k21 = 3* s2 @ s1 - 4 * s1**2 @ s2 - 4 * s1 @ s2**2 + 5 * s1**2 @ s2**2
-    # self.J = torch.stack([torch.stack([partial_delta_f1_k12, partial_delta_f1_k21]), torch.stack([partial_delta_f2_k12, partial_delta_f2_k21])])
-    # Tr_J = torch.trace(self.J)
-    # Det_J = torch.linalg.det(self.J)
-    # eigenvalues  =  0.5 * torch.stack([Tr_J + torch.sqrt(Tr_J**2 - 4 * Det_J), Tr_J - torch.sqrt(Tr_J**2 - 4 * Det_J)])
-    # A_eigenvalues = 1 + self.eta*eigenvalues
-    # self.critical_eta = (Tr_J + torch.sqrt(Tr_J**2 - 4 * Det_J))/Det_J
-    # if torch.abs(A_eigenvalues).all() < 1:
-    #   return True
-    # return False
     alpha1 = torch.mean(s1**2)
     alpha2 = torch.mean(s2**2)
     Tr = 4 * alpha1 * alpha2 - alpha1 - alpha2
     Det = - 9/16 +3*alpha1 +3*alpha2 - 4*alpha1**2 - 4 * alpha2**2 - (29/2)*alpha1*alpha2 +18*alpha1*alpha2**2 +18*alpha1**2*alpha2 - 21*alpha1**2*alpha2**2
-    # gamma_1 = torch.abs(1+0.5*self.eta*(Tr + torch.sqrt(Tr**2 - 4*Det)))
-    # gamma_2 = torch.abs(1+0.5*self.eta*(Tr - torch.sqrt(Tr**2 - 4*Det)))
     self.critical_eta = -(Tr + torch.sqrt(Tr**2 - 4 * Det))/Det
-    gammas = torch.sqrt(1+self.eta*Tr + self.eta**2*Det)
+    # gammas = torch.sqrt(1+self.eta*Tr + self.eta**2*Det)
+    gamma1 = abs(1+0.5*self.eta*(Tr + torch.sqrt(Tr**2-4*Det)))
+    gamma2 = abs(1+0.5*self.eta*(Tr - torch.sqrt(Tr**2-4*Det)))
     # if torch.abs(A_eigenvalues).all() < 1:
-    if float(gammas) < 1:
-      return True
+    if gamma1 < 1 and gamma2 < 1:
+     return True
+    # if float(gammas) < 1:
+    #   return True
     return False
 
-  # def dynamics_a(self, chi, Gamma, g_p, g_pp):
-  #   a = (chi @ Gamma) @ (g_pp/(g_p)**3)
-  #   # a1 = a[0]**2 # first differential function for unstable dynamics
-  #   # a2 = (36 * a[1]**4 - 29 * a[1]**2 + 6 + torch.sqrt(-6 * a[1]**2 * (2*a[1]**2 - 1)**3)) / (84 * a[1]**4 - 72 * a[1]**2 + 16) # second differential function for unstable dynamics
-  #   # a2_neg = (36 * a[1]**4 - 29 * a[1]**2 + 6 - torch.sqrt(-6 * a[1]**2 * (2*a[1]**2 - 1)**3)) / (84 * a[1]**4 - 72 * a[1]**2 + 16) # second (negative) differential function for unstable dynamics
-  #   # self.a_1.append(a1)
-  #   # self.a_2.append(a2)
-  #   # self.a_2_neg.append(a2_neg)
-
-  #   return  a
   def dynamics_a(self):
 
     a1 = torch.mean(self.s1)
     a2 = torch.mean(self.s2)
     # a1 = a[0]**2 # first differential function for unstable dynamics
-    a2_neg =(36 * a2**4 - 29 * a2**2 + 6 - torch.sqrt(-6 * a2**2 * (2*a2**2 - 1)**3)) / (84 * a2**4 - 72 * a2**2 + 16) # second differential function for unstable dynamics
-    a2 = (36 * a2**4 - 29 * a2**2 + 6 + torch.sqrt(-6 * a2**2 * (2*a2**2 - 1)**3)) / (84 * a2**4 - 72 * a2**2 + 16) # second differential function for unstable dynamics
+    # a2_neg =(36 * a2**4 - 29 * a2**2 + 6 - torch.sqrt(-6 * a2**2 * (2*a2**2 - 1)**3)) / (84 * a2**4 - 72 * a2**2 + 16) # second differential function for unstable dynamics
+    # a2 = (36 * a2**4 - 29 * a2**2 + 6 + torch.sqrt(-6 * a2**2 * (2*a2**2 - 1)**3)) / (84 * a2**4 - 72 * a2**2 + 16) # second differential function for unstable dynamics
     # a_1.append(a1)
     # a_2.append(a2)
     # a_2_neg.append(a2_neg)
@@ -237,7 +189,7 @@ class SimpleSynaesthesiaNet(nn.Module):
     self.variances = torch.tensor(torch.zeros(int((max_iter)/self.time_step), self.modalities))
     self.critical_etas = torch.tensor(torch.zeros(int((max_iter)/self.time_step)))
     self.x = x
-    orig_ts = torch.linspace(0., int((max_iter)/self.time_step)+1, max_iter)
+    orig_ts = torch.linspace(0., int((max_iter)/self.time_step), max_iter)
     samp_ts = orig_ts
     # s1 = self.s1.clone()
     # s2 = self.s2.clone()
@@ -252,7 +204,7 @@ class SimpleSynaesthesiaNet(nn.Module):
       if self.convergence(x, s1, s2): # steady-stae
         status.append('Stable')
         self.steady_state(x, s1, s2)
-      elif det == False:
+      elif det == 0:
         status.append('Unstable')
       else:
         status.append('Unstable')
@@ -297,7 +249,7 @@ class SimpleSynaesthesiaNet(nn.Module):
   def dynamics(self, t, s):
     # print((self.W1 @ x[0] + self.K[0][1].clone() * s[1]))
     s1, s2 = s
-    s1_dt =-s1 + self.g(self.W1 @ self.x[0] + self.K[0][1].clone() * s2)/self.tau
+    s1_dt = -s1 + self.g(self.W1 @ self.x[0] + self.K[0][1].clone() * s2)/self.tau
     s2_dt = -s2 + self.g(self.W2 @ self.x[1] + self.K[1][0].clone() * s1)/self.tau
     return torch.stack([s1_dt, s2_dt])
   def logistic_derivative(self,x):
@@ -306,62 +258,85 @@ class SimpleSynaesthesiaNet(nn.Module):
     return self.logistic_derivative(x) * (1 - 2*self.g(x))
   def third_logistic_derivative(self, x):
     return self.second_logistic_derivative(x)* (1 - 2*self.g(x)) - 2*self.logistic_derivative(x)**2
-# def run():
-#syn
-syn=[]
-ks = []
-variances = np.linspace(0.01, 0.25, 27)
-k=np.random.choice(np.linspace(-0.01,0.01),2)
-plots = []
-plots2 = []
-W1 = nn.Parameter(torch.randn(1, 1), requires_grad=True)
-W2 = nn.Parameter(torch.randn(1, 1), requires_grad=True)
-outs =[]
-vs=[]
-for i in range(len(variances[24:])):
-  for j in range(len(variances[24:])):
+def run():
+  #syn
+  syn=[]
+  ks = []
+  variances = np.linspace(0.01, 0.25, 3)
+  # Generate random angles
+  theta = 2 * np.pi * torch.rand(1)
 
-    n = tdist.Normal(0, torch.sqrt(torch.tensor([variances[i]])))
-    n2 = tdist.Normal(0, torch.sqrt(torch.tensor([variances[j]])))
-    x1 = n.sample((1,)).float()[:,0] # modality 1
-    x2 = n2.sample((1,)).float()[:,0] # modality 2 adjust variances for analysis
-    x = torch.stack([x1, x2])
+  # Set radius for the ring
+  radius = 0.1  # Can be adjusted
 
-    net = SimpleSynaesthesiaNet(np.shape(x), 1)
-    # consistent weights
-    net.K = nn.Parameter(torch.tensor([[0, k[0]], [k[1],0]]))
-    net.W1 = W1
-    net.W2 = W2
-    # print(np.shape(x), x)
-    out = net.forward(x, 1500)
-    if abs(net.K[0][1]) < 1 and abs(net.K[1][0]) < 1:
-    # if out[-1] == 'Stable':
-      plots.append([variances[i], variances[j], 1]) # no cross-talk present
-    else:
-      plots2.append([variances[i], variances[j], 0])
-      print('unstable')
-      print(net.K)
-    outs.append(out)
-    vs.append([variances[i], variances[j]])
-    ks.append(net.K)
-# df = pd.DataFrame()
-# pd.DataFrame({ks}).to_csv('EmergentSynaesthesiaks.csv')
-# pd.DataFrame({outs}).to_csv('EmergentSynaesthesiaouts.csv')
-# pd.DataFrame({plots}).to_csv('EmergentSynaesthesiapl.csv')
-# pd.DataFrame({plots2}).to_csv('EmergentSynaesthesiapl2.csv')
-# pd.DataFrame({vs}).to_csv('EmergentSynaesthesiavs.csv')
+  # Convert to cartesian coordinates
+  K12 = radius * torch.cos(theta)
+  K21 = radius * torch.sin(theta)
 
-# return plots, plots2
-def make_fig(plots, plots2):
+  plots = []
+  plots2 = []
+  plots3 = []
+  W1 = nn.Parameter(torch.randn(1, 1), requires_grad=True)
+  W2 = nn.Parameter(torch.randn(1, 1), requires_grad=True)
+  outs =[]
+  vs=[]
+  for i in range(len(variances)):
+    for j in range(len(variances)):
+
+      n = tdist.Normal(0, torch.sqrt(torch.tensor([variances[i]])))
+      n2 = tdist.Normal(0, torch.sqrt(torch.tensor([variances[j]])))
+      x1 = n.sample((1,)).float()[:,0] # modality 1
+      x2 = n2.sample((1,)).float()[:,0] # modality 2 adjust variances for analysis
+      x = torch.stack([x1, x2])
+
+      net = SimpleSynaesthesiaNet(np.shape(x), 2)
+      # consistent weights
+      net.K = nn.Parameter(torch.tensor([[0, K12], [K21,0]]))
+      net.W1 = W1
+      net.W2 = W2
+      # print(np.shape(x), x)
+      out = net.forward(x,500)
+      if torch.round(abs(net.K[0][1]), decimals=1) < 1.5 and torch.round(abs(net.K[1][0]), decimals=1) < 1.5:
+      # if out[-1] == 'Stable':
+        plots.append([variances[i], variances[j], 1]) # no cross-talk present
+      # elif out[-1] == "Unstable":
+      else:
+        if out[-1] == 'Stable' and torch.round(abs(net.K[0][1]) - abs(net.K[1][0]), decimals=1) > 1.5:
+          plots3.append([variances[i], variances[j],2])
+          print('unsure')
+          print(net.K)
+        elif out[-1] == 'Stable':
+          plots.append([variances[i], variances[j], 1]) # no cross-talk present
+          print('unsure stable')
+        else:
+          plots2.append([variances[i], variances[j], 0])
+          print('unstable')
+          print(net.K)
+      plt.plot(out)
+      outs.append(out)
+      vs.append([variances[i], variances[j]])
+      ks.append(net.K)
+      plt.show()
+  # df = pd.DataFrame()
+  # pd.DataFrame({ks}).to_csv('EmergentSynaesthesiaks.csv')
+  # pd.DataFrame({outs}).to_csv('EmergentSynaesthesiaouts.csv')
+  # pd.DataFrame({plots}).to_csv('EmergentSynaesthesiapl.csv')
+  # pd.DataFrame({plots2}).to_csv('EmergentSynaesthesiapl2.csv')
+  # pd.DataFrame({vs}).to_csv('EmergentSynaesthesiavs.csv')
+
+  return plots, plots2, plots3
+def make_fig(plots, plots2, plots3):
   for i in range(len(plots)):
     plt.scatter(plots[i][0], plots[i][1], color ='green')
   for i in range(len(plots2)):
     plt.scatter(plots2[i][0], plots2[i][1], color = 'red')
+  for i in range(len(plots3)):
+    plt.scatter(plots3[i][0], plots3[i][1], color = 'orange')    
   plt.savefig('test.jpeg')
-# plt.xlim([0, 0.25])
-# plt.ylim([0, 0.25])
-# plt.title()
-plt.xlabel('Varianance unit #1')
-plt.ylabel('Varianance unit #2')
-# plots, plots2 = run()
-make_fig(plots, plots2)
+  # plt.xlim([0, 0.25])
+  # plt.ylim([0, 0.25])
+  # plt.title()
+  plt.xlabel('Varianance unit #1')
+  plt.ylabel('Varianance unit #2')
+plots, plots2, plots3 = run()
+make_fig(plots, plots2, plots3)
