d8f4b96b19e2a8ef54a73c705c559e8a37f8406f
DnaMonkau <106347125+DnaMonkau@users.noreply.github.com>
2025-03-09 20:46:07 +0100

    Update load_images.m

--- WorkingMemory/load_images.m
+++ WorkingMemory/load_images.m
@@ -111,8 +111,8 @@ function [dimensions, images] = load_images()
         % 
         % image = image + round(Isynaesthesia * image_m);
         % randomly permeate along a gaussian distribution
-        Prob = reshape(randsample([0, 1], prod(size(image)), true, [0.5-Isynaesthesia, 0.5+Isynaesthesia]), size(image));
-        image = double(image) + double(Prob*127);
+        %Prob = reshape(randsample([0, 1], prod(size(image)), true, [0.5-Isynaesthesia, 0.5+Isynaesthesia]), size(image));
+        %image = double(image) + double(Prob*127);
         images{end + 1} = image;
     end
     dimensions = size(image);
