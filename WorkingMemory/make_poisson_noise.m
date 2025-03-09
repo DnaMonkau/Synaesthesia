f59700a7c596f802163eb4e86b1236ed831522e2
DnaMonkau <106347125+DnaMonkau@users.noreply.github.com>
2025-03-09 22:38:28 +0100

    Update make_poisson_noise.m

--- WorkingMemory/make_poisson_noise.m
+++ WorkingMemory/make_poisson_noise.m
@@ -10,12 +10,14 @@ function [I_poisson_noise] = make_poisson_noise(dimensions)
     T_poisson(T_poisson == 0) = 1;
     T_poisson = cumsum(T_poisson, 2);
     
-    I_poisson_noise = zeros(params.quantity_neurons, max(T_poisson, [], 'all'));
+    noise_length = max(max(T_poisson, [], 'all'), params.n + 1);
+    I_poisson_noise = zeros(params.quantity_neurons, noise_length, 'single');
+    
      I_poisson_noise = single( I_poisson_noise);
     for j = 1:params.quantity_neurons
         for i = 1:params.poisson_n_impulses
             be = T_poisson(j, i);
-            en = T_poisson(j, i) + params.poisson_impulse_duration;
+            en = min(be + params.poisson_impulse_duration, noise_length);
             I_poisson_noise(j, be:en) = amplitude(j, i);
         end
     end
