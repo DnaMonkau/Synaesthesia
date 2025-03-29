d5077864a3840563ecbe6441681da6b3f4e25839
DnaMonkau <106347125+DnaMonkau@users.noreply.github.com>
2025-03-29 01:50:53 +0100

    Add files via upload

--- WorkingMemory/images/izhikevich.py
+++ WorkingMemory/images/izhikevich.py
@@ -115,6 +115,227 @@ def model_parameters():
     ## Memory performance
     params['max_spikes_thr'] = 30
     return params
+params = model_parameters()
+### Standard
+class GraphemeColourSynaesthesiaNet(nn.Module):
+  def __init__(self, input_dim, M, tau=1.0,tolfun=4e-005,  eta=0.01, eta_w =0.01, cross_talk = True, modalities = 2, FF = False):
+    super(GraphemeColourSynaesthesiaNet, self).__init__()
+
+    self.modalities = input_dim[0] # Amount of modalities
+    self.cross_talk = cross_talk # True or False
+    self.FF = FF
+    # Neurons
+    self.tau =  nn.Parameter(torch.tensor(tau)) # time constant
+    self.eta_k = nn.Parameter(torch.tensor(eta))
+    self.eta_w = nn.Parameter(torch.tensor(eta_w))
+    self.input_dim = input_dim[-1]
+    self.N = self.input_dim*2
+    self.M = M
+    self.g_p_1 = 0
+    self.g_p_2 = 0
+    self.time_step = self.dt = 0.01 # steps taken in ms
+    # self.transferFunction = 2 # 1 = tanh,  2= logistic function. liberman favourite parameter.
+
+    self.critical_eta = None # critical learning rate
+    self.variance = None # output variance for modalities
+    self.tolfun = tolfun
+
+    self.g = nn.Sigmoid()
+    # debatable whether M or modalities
+    if cross_talk:
+      self.K = nn.Parameter(torch.zeros(self.M, self.M), requires_grad=True) # start at 0 fixed point or near 0
+
+      # Generate random angles
+      theta = 2 * np.pi * torch.rand(self.M**2)
+
+      # Set radius for the ring
+      radius = 0.1  # Can be adjusted
+
+      # Convert to cartesian coordinates
+      K12 = radius * torch.cos(theta)
+      with torch.no_grad():
+        self.K = nn.Parameter(torch.reshape(K12, (self.M, self.M)))
+            # self.K[j][i]= K21
+      with torch.no_grad():
+        self.K.fill_diagonal_(0) # no self connections
+    else:
+      self.K = nn.Parameter(torch.zeros(self.M, self.M), requires_grad=False) # start at 0 fixed point
+    # Initialise feedforward weights
+    if self.FF:
+      self.W = nn.Parameter(torch.randn(self.M, self.N), requires_grad=True)
+    else:
+      self.W = nn.Parameter(torch.randn(self.M, self.N), requires_grad=False)
+
+    self.s1 = torch.zeros(self.M//2, requires_grad=False) # Membrane potential or output neurons
+    self.s2 = torch.zeros(self.M//2, requires_grad=False) # Membrane potential or output neurons
+    self.optimizer = torch.optim.Adam(self.parameters(), lr=eta)
+    self.losses =  None
+    self.variances = None
+    self.critical_etas = None
+    assert self.N <= self.M, 'This is an overcomplete network use more output than input neurons'
+  def steady_state(self, x, s1, s2):
+    '''
+    The steady state interactions
+    '''
+    # Apply first order activities to the output perception s
+    s = torch.stack([self.s1, self.s2]).flatten()
+    s = self.g(torch.matmul(self.W, x) + torch.matmul(self.K, s))
+    self.s1, self.s2 = s[:self.M//2], s[self.M//2:]
+    return self.s1, self.s2
+
+  def feedforward_dynamics(self, x, t, s1, s2, samp_ts):
+    '''
+    The feedforward dynamics  of the model
+    note: original math and paper do not consider modalities
+    '''
+    s = torch.stack([s1, s2]).flatten()
+    # try:
+    # s = odeint(self.dynamics, s, samp_ts[:t+1])[-1]
+    s = self.g(torch.matmul(self.W, x) + torch.matmul(self.K, s))
+    self.s1, self.s2 = s[:self.M//2], s[self.M//2:]
+    # except:
+      # self.s1, self.s2 = s1, s2
+    return
+
+  def objective_function(self, x, s1, s2):
+      '''
+      The cost function use for estimating the
+      mutual information between the in and output neurons.
+      Is used as a loss or cost function
+
+      note: original math and paper do not consider modalities
+      does not work with  k = 0
+      '''
+      s = torch.stack([s1, s2]).flatten()
+      g_p_1 = self.logistic_derivative(torch.matmul(self.W, x) + torch.matmul(self.K, s))
+      # g_p_2 = self.logistic_derivative(self.W2@x[1] + K21 * s1)
+      G = torch.diag(g_p_1)
+      # G_2 = torch.diag(g_p_2)
+      if torch.any(self.K != 0):
+        phi = ((G)**(-1) - self.K)**(-1)
+        # phi_2 = ((G_2)**(-1) - K21)**(-1)
+        chi = (phi @ self.W)
+      else:
+        phi = G
+        # phi_2 = G_2
+        chi = phi @ self.W
+        # chi_2 = phi_2 * self.W2
+        # print(np.shape(chi1))
+      E = - 0.5 * torch.trace(torch.log(chi.T @ chi))
+      # E2 = - 0.5 * torch.trace(torch.log(chi_2.T @ chi_2))
+      return E, chi, G, phi
+
+  def learning_rule(self, x, E):
+    if self.FF:
+      w_delta = - self.eta_w * torch.autograd.grad(E, self.W, retain_graph=True)[0]
+      with torch.no_grad():
+        self.W = nn.Parameter(self.W.clone() + w_delta)
+
+    if self.cross_talk:
+      k_delta = - self.eta_k * torch.autograd.grad(E, self.K, retain_graph=True)[0]
+
+      with torch.no_grad():
+        self.K += k_delta
+        self.K.fill_diagonal_(0)
+    return
+
+
+  def stable(self, x, s1, s2):
+    alpha1 = torch.mean(s1**2)
+    alpha2 = torch.mean(s2**2)
+    Tr = 4 * alpha1 * alpha2 - alpha1 - alpha2
+    Det = - 9/16 +3*alpha1 +3*alpha2 - 4*alpha1**2 - 4 * alpha2**2 - (29/2)*alpha1*alpha2 +18*alpha1*alpha2**2 +18*alpha1**2*alpha2 - 21*alpha1**2*alpha2**2
+    self.critical_eta = -(Tr + torch.sqrt(Tr**2 - 4 * Det))/Det
+    gamma1 = abs(1+0.5*self.eta_k*(Tr + torch.sqrt(Tr**2-4*Det)))
+    gamma2 = abs(1+0.5*self.eta_k*(Tr - torch.sqrt(Tr**2-4*Det)))
+    if gamma1 < 1 and gamma2 < 1:
+     return True
+    return False
+  #   return Det
+  def convergence(self, s1_prev, s2_prev, s1, s2, loss, i):
+    diff1 = abs(s1 - s1_prev)
+    diff2 = abs(s2 - s2_prev)
+    # difference  = torch.mean(torch.hstack([diff1, diff2])) < self.tolfun
+    difference = abs(self.losses[i-1]- loss) < self.tolfun
+    return difference.all()
+
+  def forward(self, x, max_iter=10):
+    status = []
+    self.timeline = int((max_iter)/self.time_step)
+    self.spikes= torch.tensor(torch.zeros(int((max_iter)/self.time_step), self.modalities, self.N ))
+    self.losses = torch.tensor(torch.zeros(int((max_iter)/self.time_step)))
+    self.variances = torch.tensor(torch.zeros(int((max_iter)/self.time_step), self.modalities))
+    self.critical_etas = torch.tensor(torch.zeros(int((max_iter)/self.time_step)))
+    x = x.flatten()
+    self.x =x
+    sample_ts = torch.linspace(0., int((max_iter)/self.time_step), int(max_iter/self.time_step))
+    converged = []
+    s1_prev, s2_prev = -np.inf, -np.inf
+
+
+    for i in tqdm(range(1,int((max_iter/self.time_step)))):
+     
+
+      with torch.no_grad():
+        s1 = self.s1.clone()
+        s2 = self.s2.clone()
+
+
+        if (abs(self.K) <= 1).all():
+          status.append('Stable')
+          s1, s2 = self.steady_state(x, s1, s2)
+          spiked, fired = self.Izhikevich_neurons.step(s1, s2, self.time_step)
+        else:
+          status.append('Unstable')
+
+      self.feedforward_dynamics(x, i, s1, s2, samp_ts)
+
+      loss, chi, G, phi = self.objective_function(x, s1, s2)
+      if self.convergence( s1_prev, s2_prev, s1, s2, loss, i): # steady-stae
+        converged.append(i)
+        self.FF = False
+      self.learning_rule(x, loss)
+
+
+      self.losses[i] = loss
+      loss.backward()
+
+      self.optimizer.step()
+      self.optimizer.zero_grad()
+
+      self.stable(x, s1, s2)
+
+      self.variances[i,0] = torch.mean(self.s1**2) - torch.mean(self.s1)**2
+      self.variances[i,1] = torch.mean(self.s2**2) - torch.mean(self.s2)**2
+      self.critical_etas[i] = self.critical_eta
+     # self.critical_etas[i] = self.critical_eta
+      s1_prev, s2_prev = s1.clone(), s2.clone()
+      with torch.no_grad():
+        self.K.fill_diagonal_(0)
+    # if len(converged) != 0:
+    #   print('\n --- \n Converged at iteration: ', converged[0], '\n --- \n')
+    #   # print('\n --- \n Diverged or reached max iterations after at iteration: ', converged[-1], '\n --- \n')
+    # else:
+    #   print('\n --- Did not converge \n ---\n ')
+    return status, converged
+
+  def predict(self, x, s):
+    return self.g(torch.matmul(self.W, x) + torch.matmul(self.K, s))
+
+  #helper functions
+  def dynamics(self, t, s):
+    # print((self.W1 @ x[0] + self.K[0][1].clone() * s[1]))
+    s_dt = -s + self.g(self.W @ self.x + self.K @ s)/self.tau
+    s1_dt, s2_dt = s_dt[:self.M//2], s_dt[self.M//2:]
+    return torch.stack([s1_dt, s2_dt]).flatten()
+  def logistic_derivative(self,x):
+    return self.g(x) * (1 - self.g(x))
+  def second_logistic_derivative(self, x):
+    return self.logistic_derivative(x) * (1 - 2*self.g(x))
+  def third_logistic_derivative(self, x):
+    return self.second_logistic_derivative(x)* (1 - 2*self.g(x)) - 2*self.logistic_derivative(x)**2
+
+### Izhikevich
 class IzhikevichNeuron():
   def __init__(self, params, M, N):
     super(IzhikevichNeuron, self).__init__()
@@ -389,51 +610,70 @@ class GraphemeColourSynaesthesiaSpikeNet(nn.Module):
     return self.logistic_derivative(x) * (1 - 2*self.g(x))
   def third_logistic_derivative(self, x):
     return self.second_logistic_derivative(x)* (1 - 2*self.g(x)) - 2*self.logistic_derivative(x)**2
-# flat gray scale number array
+### Simulation
+def train(Izhikevich=True):
+  img = cv2.imread('zero.jpg')
+  img = cv2.resize(img, (0,0), fx=0.06, fy=0.06)
 
-img = cv2.imread('zero.jpg')
-img = cv2.resize(img, (0,0), fx=0.25, fy=0.25)
-
-x1 = torch.from_numpy((cv2.cvtColor(img, cv2.COLOR_BGR2GRAY).flatten()<127).astype('float32'))
-# colour category per pixel 0=original  blue=green
-x2 = torch.from_numpy(np.random.choice([0,1,2, 3], size = len(x1)).astype('float32'))
-x2[x1==0] = 0
-simulation_emergence_data = torch.stack([x1, x2])
-print(len(simulation_emergence_data))
-E_Network = GraphemeColourSynaesthesiaSpikeNet(params, np.shape(simulation_emergence_data), M=len(x1)*2)
-emergence_iterations=40
-def train():
-  i=0
+  x1 = torch.from_numpy((cv2.cvtColor(img, cv2.COLOR_BGR2GRAY).flatten()<127).astype('float32'))
+  # colour category per pixel 0=original  blue=green
+  x2 = torch.from_numpy(np.random.choice([0,1,2, 3], size = len(x1)).astype('float32'))
+  x2[x1==0] = 0
+  simulation_emergence_data = torch.stack([x1, x2])
+  print(np.shape(simulation_emergence_data))
+  E_Network = GraphemeColourSynaesthesiaSpikeNet(params, np.shape(simulation_emergence_data), M=len(x1)*2)
+  #syn
+  weights1 = E_Network.W
+  emergence_iterations=20
+  Isynaesthesias = torch.zeros(10)
+  i = 0
+  bw = image_names = [
+          'zero.jpg', 'one.jpg', 'two.jpg', 'three.jpg', 'four.jpg',
+          'five.jpg', 'six.jpg', 'seven.jpg', 'eight.jpg', 'nine.jpg'
+      ]
   for file in os.listdir():
-    i+=1
-    if i>10:
-      break
-    if 'jpg' in file:
+    if file in bw:
       img = cv2.imread(file)
-      img = cv2.resize(img, (0,0), fx=0.25, fy=0.25)
+      img = cv2.resize(img, (0,0), fx=0.06, fy=0.06)
       x1 = torch.from_numpy((cv2.cvtColor(img, cv2.COLOR_BGR2GRAY).flatten()<127).astype('float32'))
       # colour category per pixel 0=original  blue=green
-      x2 = torch.from_numpy(np.random.choice([0,1,2, 3], size = len(x1)).astype('float32'))
-      x2[x1==0] = 0
+      x2 = torch.from_numpy(np.random.choice([0, 1, 2, 3], size = len(x1)).astype('float32'))
+      x2[x1 == 0] = 0
+
       simulation_emergence_data = torch.stack([x1, x2])
       #syn
-      E_Network = GraphemeColourSynaesthesiaSpikeNet(params, np.shape(simulation_emergence_data), M=len(x1)*2)
+      if Izhikevich:
+        E_Network = GraphemeColourSynaesthesiaSpikeNet(params, np.shape(simulation_emergence_data), M=len(x1)*2)
 
-      weights1 = E_Network.W
-      E_Network.forward(simulation_emergence_data, max_iter = emergence_iterations)
-      print('Finalised synaesthetic simulations')
-      # non syn
-      E_Network_non = GraphemeColourSynaesthesiaSpikeNet(params, np.shape(simulation_emergence_data), M=len(x1)*2)
-      E_Network_non.W = weights1
-      E_Network_non.cross_talk = False
-      E_Network_non.forward(simulation_emergence_data, max_iter = emergence_iterations)
-      print('Finalised non-synaesthetic simulations')
+        E_Network.W = weights1
+        E_Network.forward(simulation_emergence_data, max_iter = emergence_iterations)
+        print('Finalised synaesthetic simulations')
+        # non syn
+        E_Network_non = GraphemeColourSynaesthesiaSpikeNet(params, np.shape(simulation_emergence_data), M=len(x1)*2)
+        E_Network_non.W = weights1
+        E_Network_non.cross_talk = False
+        E_Network_non.forward(simulation_emergence_data, max_iter = emergence_iterations)
+        print('Finalised non-synaesthetic simulations')
+      else:
+        E_Network = GraphemeColourSynaesthesiaNet(params, np.shape(simulation_emergence_data), M=len(x1)*2)
+
+        E_Network.W = weights1
+        E_Network.forward(simulation_emergence_data, max_iter = emergence_iterations)
+        print('Finalised synaesthetic simulations')
+        # non syn
+        E_Network_non = GraphemeColourSynaesthesiaNet(params, np.shape(simulation_emergence_data), M=len(x1)*2)
+        E_Network_non.W = weights1
+        E_Network_non.cross_talk = False
+        E_Network_non.forward(simulation_emergence_data, max_iter = emergence_iterations)
+        print('Finalised non-synaesthetic simulations')
       # Calculate Synaesthetic Baseline
       Synaesthesia_s = torch.stack([E_Network.s1, E_Network_non.s1])
       Non_Synaesthesia_s = torch.stack([E_Network.s2, E_Network_non.s2])
 
       Isynaesthesia = torch.mean(abs(Synaesthesia_s -  Non_Synaesthesia_s), 0).mean()  # synaesthesia output current I
-      Isynaesthesia = Isynaesthesia
+      Isynaesthesias[i] = Isynaesthesia.detach().numpy()
       print('Synaesthetic Baseline:', Isynaesthesia)
-      # # WM
-train()
+      i+=1
+  return Isynaesthesias
+Isynaesthesias = train(True)
+torch.save(Isynaesthesias, 'Izhikevich_number_color_Synaesthesia.pt')
